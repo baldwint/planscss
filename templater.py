a38ab2c3d8dab4a57c1f2946d5390f6c117ab9ec
Thomas Baldwin <tbaldwin@uoregon.edu>
2013-05-24 03:28:15 -0700

    ordered dictionary

--- templater.py
+++ templater.py
@@ -11,7 +11,11 @@ template = Template(open('index.html').read())
 
 # each stylesheet has its own folder within the build directory
 import json
-sheets = json.load(open('sheets.json')).keys()
+from collections import OrderedDict
+sheets = json.load(
+        open('sheets.json'),
+        object_pairs_hook=OrderedDict
+    ).keys()
 
 # pages to show. exclude customize since that screenshot is wrong
 pages = ['read', 'edit', 'search', 'styles', 'editsubmit', 'nosuchuser']
