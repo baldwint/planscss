d87e120aa3a6a2651acc6574fe4258fc2357650a
Thomas Baldwin <tbaldwin@uoregon.edu>
2013-05-24 02:55:44 -0700

    make templater use json

--- templater.py
+++ templater.py
@@ -10,8 +10,8 @@ from jinja2 import Template
 template = Template(open('index.html').read())
 
 # each stylesheet has its own folder within the build directory
-sheets = [fn for fn in os.listdir('screenshots')
-        if os.path.isdir(os.path.join('screenshots', fn))]
+import json
+sheets = json.load(open('sheets.json')).keys()
 
 # pages to show. exclude customize since that screenshot is wrong
 pages = ['read', 'edit', 'search', 'styles', 'editsubmit', 'nosuchuser']
